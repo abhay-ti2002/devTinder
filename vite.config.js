238ce65f302e50070f0f3de6683410042f4e5ff1
abhay-ti2002 <abhi.tiwari2027@gmail.com>
2025-12-22 22:10:09 +0530

    upadate VITE CONFIG

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // allows 0.0.0.0
+  },
+  preview: {
+    host: true, // allows external hosts
+    allowedHosts: ["devtinder-2-tzca.onrender.com"], // add your Render URL
+  },
+});
